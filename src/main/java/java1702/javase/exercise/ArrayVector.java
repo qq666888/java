27c9559d5b4aa0a4f6255cf85603d6a28a58469b
qq666888 <1096161287@qq.com>
2017-03-31 07:55:34 +0800

    shape

---
+++ src/main/java/java1702/javase/exercise/ArrayVector.java
@@ -0,0 +1,49 @@
+package java1702.javase.exercise;
+
+/**
+ * Created by $qiqi
+ * on 2017/3/30.
+ * java
+ */
+public class ArrayVector { // 使用数组来模拟一个向量 DynamicArray
+    private static final int DEFAULT_CAPACITY = 10;
+    private String[] strings;
+    private int size;
+
+    public ArrayVector() {
+        strings = new String[DEFAULT_CAPACITY];
+    }
+
+    public ArrayVector(int initialCapacity) {
+        strings = new String[initialCapacity];
+    }
+
+    public void add(String string) {
+        strings[size] = string;
+        size++;
+    }
+
+    public String get(int index) {
+        return strings[index];
+    }
+
+    public static void main(String[] args) {
+        // void add(String element)
+        // String remove(int index)
+        // String set(int index, String element)
+        // String get(int index)
+        // int size()
+        // int capacity()
+
+        ArrayVector arrayVector = new ArrayVector();
+        arrayVector.add("hello");
+        arrayVector.add("hi");
+        System.out.println(arrayVector.get(0)); // hello
+        System.out.println(arrayVector.get(1)); // hi
+    }
+
+    /*
+         |-----10------|
+         |----------20----------|
+     */
+}
