77af35825663513e464bb9a41e6f517897bdf64a
qq666888 <1096161287@qq.com>
2017-04-07 18:44:05 +0800

    shape

---
+++ src/main/java/java1702/javase/collection/Sar.java
@@ -0,0 +1,22 @@
+package java1702.javase.collection;
+
+/**
+ * Created by $qiqi
+ * on 2017/4/7.
+ * java
+ */
+public class Sar {
+    public static void main(String[] args) {
+
+        String s="hello";
+        System.out.println(s.charAt(1));
+        System.out.println(s.concat("hi"));
+        System.out.println(s.contains("s"));
+        System.out.println(s.endsWith("o"));
+        System.out.println(s.equals("hi"));
+        System.out.println(String.format(s,"hi"));
+        System.out.println(s.getBytes());
+        System.out.println(s.toCharArray());
+
+    }
+}
--- src/main/java/java1702/javase/exam3/E2.java
+++ src/main/java/java1702/javase/exam3/E2.java
@@ -9,24 +9,43 @@ import java.util.Scanner;
  */
 public class E2 {
     public static void main(String[] args){
-       StringBuffer s1 = new StringBuffer();
+//       StringBuffer s1 = new StringBuffer();
+//
+//        String s2 = "abcDEF";
+//
+//
+//        char c[] = s2.toCharArray();
+//
+//
+//        for(int i = 0; i<s2.length(); i++){if(c[i]>=97){
+//            s1.append((c[i]+"").toUpperCase());
+//        }
+//
+//        else{
+//            s1.append((c[i]+"").toLowerCase());
+//        }
+//
+//        }
+//        System.out.println(s2);
+//        System.out.println(s1);
+        Scanner scanner=new Scanner(System.in);
+        String s=scanner.nextLine();
+        System.out.println(s);
+        char[] chars=s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char aChar = chars[i];
+            if(aChar>='A'&& aChar<='Z'){
+                chars[i]=(char)(aChar+32);
+            }
+            if(aChar>='A'&& aChar<='Z'){
+                chars[i]=(char)(aChar-32);
+            }
 
-        String s2 = "abcDEF";
-
-
-        char c[] = s2.toCharArray();
-
-
-        for(int i = 0; i<s2.length(); i++){if(c[i]>=97){
-            s1.append((c[i]+"").toUpperCase());
         }
-
-        else{
-            s1.append((c[i]+"").toLowerCase());
-        }
-
-        }
-        System.out.println(s2);
-        System.out.println(s1);
+        char temp=chars[0];
+        chars[0]=chars[chars.length-1];
+        chars[chars.length-1]=temp;
+        System.out.println(String.valueOf(chars));
+        System.out.println(new String(chars));
     }
 }
